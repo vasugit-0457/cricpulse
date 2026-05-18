b7cac67d14e70717c76496ed69032582902916ec
vasugit-0457 <vasusharma201213@gmail.com>
2026-05-18 21:44:47 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,8 @@
 // js/supabase.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = 'YOUR_SUPABASE_URL';       // Replace with your project URL
-const SUPABASE_ANON_KEY = 'YOUR_ANON_KEY';      // Replace with your anon key
+const SUPABASE_URL = 'https://mlbuojlplqagyhjnjhpt.supabase.co/rest/v1/';       // Replace with your project URL
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sYnVvamxwbHFhZ3loam5qaHB0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwOTU0NzAsImV4cCI6MjA5NDY3MTQ3MH0.w2cNFuFqvE3S8YxOyfB6XSY6QdjMm206aUY3gNcafbc';      // Replace with your anon key
 
 export const supaClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
